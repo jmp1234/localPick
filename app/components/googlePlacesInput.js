d51edd7fb96d49d835b27f5c94bedbb8587ac9e3
jmp1234 <johnnypham944@yahoo.com>
2019-06-23 19:35:04 -0700

    installed google places api the city can now be found and entered in the local picks page

---
+++ app/components/googlePlacesInput.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import config from '../../config/config';
+import { withNavigation } from 'react-navigation';
+
+const homePlace = { description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
+const workPlace = { description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
+
+export default withNavigation((props) => {
+  return (
+    <GooglePlacesAutocomplete
+      placeholder='Enter city'
+      minLength={2} // minimum length of text to search
+      autoFocus={false}
+      returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+      listViewDisplayed='auto'    // true/false/undefined
+      fetchDetails={true}
+      renderDescription={row => row.description} // custom description render
+      onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+        // console.log(data, details);
+        console.log('data: ', data)
+        console.log('details: ', details)
+        console.log('city: ', details.address_components[0].long_name)
+        props.navigation.navigate('LocalPicks', {city: details.address_components[0].long_name})
+      }}
+
+      getDefaultValue={() => ''}
+
+      query={{
+        // available options: https://developers.google.com/places/web-service/autocomplete
+        key: config.GOOGLE_PLACES_KEY,
+        language: 'en', // language of the results
+        types: '(cities)' // default: 'geocode'
+      }}
+
+      styles={{
+        textInputContainer: {
+          width: '100%'
+        },
+        description: {
+          fontWeight: 'bold'
+        },
+        predefinedPlacesDescription: {
+          color: '#1faadb'
+        }
+      }}
+
+      currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+      currentLocationLabel="Current location"
+      nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+      GoogleReverseGeocodingQuery={{
+        // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+      }}
+      GooglePlacesSearchQuery={{
+        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+        rankby: 'distance',
+        types: 'food'
+      }}
+
+      filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+      // predefinedPlaces={[homePlace, workPlace]}
+
+      debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+      // renderLeftButton={()  => <Text>Left</Text>}
+      // renderRightButton={() => <Text>Custom text after the input</Text>}
+    />
+  );
+})
--- app/screens/search.js
+++ app/screens/search.js
@@ -1,41 +1,63 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import GooglePlaceInput from '../components/googlePlacesInput';
 
 class Search extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      location: ''
+      location: '',
+      page: 0,
     }
   }
 
   enterLocation = () => {
-    this.props.navigation.navigate('LocalPicks')
+    //go to local picks page:
+    // this.props.navigation.navigate('LocalPicks')
+
+    //go to the google places input field:
+    this.setState({
+      page: 1
+    })
   }
 
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 30, marginBottom: 30}}>Find your local pick</Text>
-        <TextInput
-          editable={true}
-          placeholder={'Enter city or address'}
-          onChangeText={(text) => this.setState({location: text})}
-          value={this.state.location}
-          style={{width: 250, marginVertical: 10, padding: 5, borderWidth: 1, borderColor: 'grey', borderRadius: 3}}
-        />
-        <TouchableOpacity
-          style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'blue', borderRadius: 5}}
-          onPress={() => this.enterLocation()}>
-          <Text style={{color: 'white'}}>Find Local Picks</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'blue', borderRadius: 5}}
-          onPress={() => this.props.navigation.navigate('UserAuth')}>
-          <Text style={{color: 'white'}}>Log In</Text>
-        </TouchableOpacity>
-      </View>
+      <Fragment>
+      {this.state.page === 0 ? (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontSize: 30, marginBottom: 30}}>Find your local pick</Text>
+          <TouchableOpacity
+            onPress={() => this.enterLocation()}>
+            <TextInput
+              editable={false}
+              pointerEvents="none"
+              placeholder={'Enter your location'}
+              onChangeText={(text) => this.setState({location: text})}
+              value={this.state.location}
+              style={{width: 250, marginVertical: 10, padding: 5, borderWidth: 1, borderColor: 'grey', borderRadius: 3}}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'blue', borderRadius: 5}}
+            onPress={() => this.props.navigation.navigate('UserAuth')}>
+            <Text style={{color: 'white'}}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={{ backgroundColor: 'darkgrey', paddingTop: 20, flex: 1 }}>
+          <View style={{flexDirection: 'row',paddingTop: 30, alignItems: 'center', justifyContent: 'space-between'}}>
+            <TouchableOpacity
+              onPress={() => this.setState({page: 0})}
+              style={{width:100}}>
+              <Text style={{fontSize:24, fontWeight: 'bold', paddingLeft: 10, color: 'white'}}>✖︎</Text>
+            </TouchableOpacity>
+          </View>
+          <GooglePlaceInput />
+        </View>
+      )}
+    </Fragment>
     )
   }
 }
