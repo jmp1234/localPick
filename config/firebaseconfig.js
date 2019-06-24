9b9dc5a596d06f72ed3b9a1d5c9aec781638ca59
jmp1234 <johnnypham944@yahoo.com>
2019-06-24 14:44:05 -0700

    set up firebase config file

---
+++ config/firebaseconfig.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+import config from './config';
+
+// Initialize Firebase
+firebase.initializeApp(config.firebaseConfig);
+
+export const f = firebase;
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
